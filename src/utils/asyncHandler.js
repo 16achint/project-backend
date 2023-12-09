3dd1c7a95157281302b4cb8ecbd148793eb8a67c
achint <achintj96244@gmail.com>
2023-12-10 02:00:07 +0530

    utils code implementation

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+// method 1
+
+const asyncHandler = (requestHanlder) => {
+  (req, res, next) => {
+    Promise.resolve(requestHanlder(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () = {}
+// const asyncHandler = (func) =>  => {}
+// const asyncHandler = (fnnc) => async () => {}
+
+// method 2
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
