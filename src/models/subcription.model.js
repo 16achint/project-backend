cc92a2fda6f7d82929d3e2ffd8b1efce81da2554
achint <achintj96244@gmail.com>
2024-01-06 21:53:56 +0530

    fix logout functionlity and update subcription model

--- src/models/subcription.model.js
+++ src/models/subcription.model.js
@@ -1,14 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({
-  subscriber: {
-    type: Schema.ObjectId,
-    ref: "User",
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.ObjectId,
+      ref: "User",
+    },
   },
-  channel: {
-    type: Schema.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 export const subscription = mongoose.model("subscription", subscriptionSchema);
