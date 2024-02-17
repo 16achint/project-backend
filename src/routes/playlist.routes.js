c49ff426902ebd80bd9aee261316f77ec5df3577
achint <achintj96244@gmail.com>
2024-02-17 18:01:25 +0530

    update playlist routes

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,7 +1,29 @@
 import { Router } from "express";
-import { createPlaylist } from "../controllers/playlist.controller";
+import {
+  createPlaylist,
+  getUserPlayList,
+  getPlaylistbyId,
+  addVideotoPlaylist,
+  removeVideoFromPlaylist,
+  deletePlayList,
+  updatePlaylist,
+} from "../controllers/playlist.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
-const router = Router();
 
+const router = Router();
 router.use(verifyJWT);
+
 router.route("/").post(createPlaylist);
+
+router
+  .route("/:playlistId")
+  .get(getPlaylistbyId)
+  .patch(updatePlaylist)
+  .delete(deletePlayList);
+
+router.route("/user/:userId").get(getUserPlayList);
+
+router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+router.route("/add/:videoId/:playlistId").patch(addVideotoPlaylist);
+
+export default router;
