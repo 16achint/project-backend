5dbdbc97d947fad9f91ce7615a312367de33f3bb
achint <achintj96244@gmail.com>
2024-02-26 13:06:29 +0530

    update multer middleware and video routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -16,9 +16,9 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/uploadVideo").post(
+router.route("/").post(
   verifyJWT,
-  (next) => next(),
+  // (next) => next,
   upload.fields([
     { name: "videoFile", maxCount: 1 },
     { name: "thumbnail", maxCount: 1 },
