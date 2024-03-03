493a6ae7db8e435827df1f7d93363bcb623de290
achint <achintj96244@gmail.com>
2024-03-03 17:19:27 +0530

    add playlist router

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 import subscriptionRouter from "./routes/subcription.routes.js";
 
 // route declaration
@@ -32,5 +33,6 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/playlist", playlistRouter);
 
 export { app };
