53c823c79ce4a6435d040044b3f17f5a38a9f8c5
achint <achintj96244@gmail.com>
2024-03-04 23:21:31 +0530

    add dashboard routes

---
+++ src/routes/dashboard.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  getChannelStates,
+  getChannelVideos,
+} from "../controllers/dashboard.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/states").get(getChannelStates);
+router.route("/video").get(getChannelVideos);
+
+export default router;
